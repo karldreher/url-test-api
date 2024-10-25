c46dbc4aeb1ce0e1e4f2c50c2be0dc15298c629c
Karl Dreher <karldreher@outlook.com>
2024-10-24 20:26:55 -0700

    docs: update comments

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,14 @@
 /**
- * An interface which allows for a URL (as a string) and a status, e.g. 200.
+ * An interface for a URL (as a string) and a status, e.g. 200.
  */
 export interface URLStatus {
   url: string;
   status: number;
 }
 
+/**
+ * An interface for a URLStatusRequest, which is an ID.
+ */
 export interface URLStatusRequest {
   id: string;
 }
